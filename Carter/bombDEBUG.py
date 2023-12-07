8c81842f1938a68fbc8225ca8d92e6debf6d16b6
Ben <carter.murawski@spartans.ut.edu>
2023-12-06 23:55:07 -0500

    timer issue?

--- Carter/bombDEBUG.py
+++ Carter/bombDEBUG.py
@@ -87,7 +87,7 @@ class Lcd(Frame):
     # sets up the LCD "boot" GUI
     def setupBoot(self):
         self.erase()
-        self.phase = 2
+        print("hello")
         # set column weights
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=2)
@@ -100,6 +100,7 @@ class Lcd(Frame):
 
     # sets up the LCD GUI
     def setup(self):
+        print("Nigga")
         # the timer
         self._ltimer = Label(self, bg="black", fg="#00ff00", font=("Courier New", 18), text="Time left: ")
         self._ltimer.grid(row=1, column=0, columnspan=3, sticky=W)
@@ -125,7 +126,7 @@ class Lcd(Frame):
             # the quit button
             self._bquit = tkinter.Button(self, bg="red", fg="white", font=("Courier New", 18), text="Quit", anchor=CENTER, command=self.quit)
             self._bquit.grid(row=6, column=2, pady=40)
-            
+        self.phase = 2
     def obamaDisplay(self):
         self.erase()
         self.phase = 3
@@ -459,6 +460,7 @@ def bootup(n=0):
         if (not ANIMATE):
             gui._lscroll["text"] = boot_text.replace("\x00", "")
         # configure the remaining GUI widgets
+        print("fuck")
         gui.setup()
         # setup the phase threads, execute them, and check their statuses
         if (RPi):
@@ -470,7 +472,7 @@ def bootup(n=0):
             gui._lscroll["text"] += boot_text[n]
 
         # scroll the next character after a slight delay (\x00 is a longer delay)
-        gui.after(25 if boot_text[n] != "\x00" else 750, bootup, n + 1)
+        #gui.after(25 if boot_text[n] != "\x00" else 750, bootup, n + 1)
 
 # sets up the phase threads
 def setup_phases():
@@ -513,108 +515,109 @@ def check_phases():
     if (not exploding and not pygame.mixer.music.get_busy()):
         pygame.mixer.music.load(TICK)
         pygame.mixer.music.play(-1)
-        
-    # check the timer
-    if (timer._running):
-        # update the GUI
-        gui._ltimer["text"] = f"Time left: {timer}"
-        # play the exploding audio at t-10s
-        if (not exploding and timer._interval * timer._value <= 11.25):
-            exploding = True
-            component_7seg.blink_rate = 1
-            pygame.mixer.music.load(EXPLODING)
-            pygame.mixer.music.play(1)
-        if (timer._value == 60):
-            gui._ltimer["fg"] = "#ff0000"
-    else:
-        # the countdown has expired -> explode!
-        # turn off the bomb and render the conclusion GUI
-        turn_off()
-        gui.after(100, gui.conclusion, exploding, False)
-        # don't check any more phases
-        return
-    # check the keypad
-    if (keypad._running):
-        # update the GUI
-        gui._lkeypad["text"] = f"Combination: {keypad}"
-        # the phase is defused -> stop the thread
-        if (keypad._defused):
-            keypad._running = False
-            gui._lkeypad["fg"] = "#00ff00"
-            defused()
-        # the phase has failed -> strike
-        elif (keypad._failed):
-            strike()
-            # reset the keypad
-            keypad._failed = False
-            keypad._value = ""
-    # check the wires
-    if (wires._running):
-        # update the GUI
-        gui._lwires["text"] = f"Wires: {wires}"
-        # the phase is defused -> stop the thread
-        if (wires._defused):
-            wires._running = False
-            gui._lwires["fg"] = "#00ff00"
-            defused()
-        # the phase has failed -> strike
-        elif (wires._failed):
-            strike()
-            # reset the wires
-            wires._failed = False
-    # check the button
-    if (button._running):
-        # update the GUI
-        gui._lbutton["text"] = f"Button: {button}"
-        # the phase is defused -> stop the thread
-        if (button._defused):
-            button._running = False
-            gui._lbutton["fg"] = "#00ff00"
-            defused()
-        # the phase has failed -> strike
-        elif (button._failed):
-            strike()
-            # reset the button
-            button._failed = False
-    # check the toggles
-    if (toggles._running):
-        # update the GUI
-        gui._ltoggles["text"] = f"Toggles: {toggles}"
-        # the phase is defused -> stop the thread
-        if (toggles._defused):
-            toggles._running = False
-            gui._ltoggles["fg"] = "#00ff00"
-            defused()
-        # the phase has failed -> strike
-        elif (toggles._failed):
-            strike()
-            # reset the toggles
-            toggles._failed = False
+    if gui.phase == 2:   
+        # check the timer
+        if (timer._running):
+            # update the GUI
+                #
+            gui._ltimer["text"] = f"Time left: {timer}"
+            # play the exploding audio at t-10s
+            if (not exploding and timer._interval * timer._value <= 11.25):
+                exploding = True
+                component_7seg.blink_rate = 1
+                pygame.mixer.music.load(EXPLODING)
+                pygame.mixer.music.play(1)
+            if (timer._value == 60) and gui.phase == 2:
+                gui._ltimer["fg"] = "#ff0000"
+        else:
+            # the countdown has expired -> explode!
+            # turn off the bomb and render the conclusion GUI
+            turn_off()
+            gui.after(100, gui.conclusion, exploding, False)
+            # don't check any more phases
+            return
+        # check the keypad
+        if (keypad._running):
+            # update the GUI
+            gui._lkeypad["text"] = f"Combination: {keypad}"
+            # the phase is defused -> stop the thread
+            if (keypad._defused):
+                keypad._running = False
+                gui._lkeypad["fg"] = "#00ff00"
+                defused()
+            # the phase has failed -> strike
+            elif (keypad._failed):
+                strike()
+                # reset the keypad
+                keypad._failed = False
+                keypad._value = ""
+        # check the wires
+        if (wires._running):
+            # update the GUI
+            gui._lwires["text"] = f"Wires: {wires}"
+            # the phase is defused -> stop the thread
+            if (wires._defused):
+                wires._running = False
+                gui._lwires["fg"] = "#00ff00"
+                defused()
+            # the phase has failed -> strike
+            elif (wires._failed):
+                strike()
+                # reset the wires
+                wires._failed = False
+        # check the button
+        if (button._running):
+            # update the GUI
+            gui._lbutton["text"] = f"Button: {button}"
+            # the phase is defused -> stop the thread
+            if (button._defused):
+                button._running = False
+                gui._lbutton["fg"] = "#00ff00"
+                defused()
+            # the phase has failed -> strike
+            elif (button._failed):
+                strike()
+                # reset the button
+                button._failed = False
+        # check the toggles
+        if (toggles._running):
+            # update the GUI
+            gui._ltoggles["text"] = f"Toggles: {toggles}"
+            # the phase is defused -> stop the thread
+            if (toggles._defused):
+                toggles._running = False
+                gui._ltoggles["fg"] = "#00ff00"
+                defused()
+            # the phase has failed -> strike
+            elif (toggles._failed):
+                strike()
+                # reset the toggles
+                toggles._failed = False
 
-    # note the strikes on the GUI
-    gui._lstrikes["text"] = f"Strikes left: {strikes_left}"
-    # too many strikes -> explode!
-    if (strikes_left == 0):
-        # turn off the bomb and render the conclusion GUI
-        turn_off()
-        gui.after(1000, gui.conclusion, exploding, False)
-        # stop checking phases
-        return
-    # a few strikes left -> timer goes twice as fast!
-    elif (strikes_left == 2 and not exploding):
-        timer._interval = 0.5
-        gui._lstrikes["fg"] = "#ff0000"
-    # one strike left -> timer goes even faster!
-    elif (strikes_left == 1 and not exploding):
-        timer._interval = 0.25
+        # note the strikes on the GUI
+        gui._lstrikes["text"] = f"Strikes left: {strikes_left}"
+        # too many strikes -> explode!
+        if (strikes_left == 0):
+            # turn off the bomb and render the conclusion GUI
+            turn_off()
+            gui.after(1000, gui.conclusion, exploding, False)
+            # stop checking phases
+            return
+        # a few strikes left -> timer goes twice as fast!
+        elif (strikes_left == 2 and not exploding):
+            timer._interval = 0.5
+            gui._lstrikes["fg"] = "#ff0000"
+        # one strike left -> timer goes even faster!
+        elif (strikes_left == 1 and not exploding):
+            timer._interval = 0.25
 
-    # the bomb has been successfully defused!
-    if (active_phases == 0):
-        # turn off the bomb and render the conclusion GUI
-        turn_off()
-        gui.after(100, gui.conclusion, exploding, True)
-        # stop checking phases
-        return
+        # the bomb has been successfully defused!
+        if (active_phases == 0):
+            # turn off the bomb and render the conclusion GUI
+            turn_off()
+            gui.after(100, gui.conclusion, exploding, True)
+            # stop checking phases
+            return
 
     # check the phases again after a slight delay
     gui.after(100, check_phases)
@@ -673,8 +676,7 @@ strikes_left = NUM_STRIKES
 active_phases = NUM_PHASES
 exploding = False
 
-# "boot" the bomb
-gui.after(1000, bootup)
+
 
 # display the LCD GUI
 window.mainloop()
